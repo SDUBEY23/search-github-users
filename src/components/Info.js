2ebb5165212220edd16460e494dd4c849980ac3a
Swapnil <swapnil.vishu.7027@gmail.com>
2021-04-10 02:02:47 +0530

    followers component added

--- src/components/Info.js
+++ src/components/Info.js
@@ -1,57 +1,58 @@
-import React, { useContext } from 'react';
-import { GithubContext } from '../context/context';
-import styled from 'styled-components';
-import { GoRepo, GoGist } from 'react-icons/go';
-import { FiUsers, FiUserPlus } from 'react-icons/fi';
+import React, { useContext } from "react";
+import { GithubContext } from "../context/context";
+import styled from "styled-components";
+import { GoRepo, GoGist } from "react-icons/go";
+import { FiUsers, FiUserPlus } from "react-icons/fi";
 
 const UserInfo = () => {
-  const {githubUser} = useContext(GithubContext);
-  const {public_repos, followers, following, public_gists} = githubUser;
-  
+  const { githubUser } = useContext(GithubContext);
+  const { public_repos, followers, following, public_gists } = githubUser;
+
   const items = [
     {
       id: 1,
       icon: <GoRepo className="icon" />,
-      label: 'repo',
+      label: "repo",
       value: public_repos,
-      color: 'pink'
+      color: "pink",
     },
     {
       id: 2,
       icon: <FiUsers className="icon" />,
-      label: 'followers',
+      label: "followers",
       value: followers,
-      color: 'green'
+      color: "green",
     },
     {
       id: 3,
       icon: <FiUserPlus className="icon" />,
-      label: 'following',
+      label: "following",
       value: following,
-      color: 'purple'
+      color: "purple",
     },
     {
       id: 4,
       icon: <GoGist className="icon" />,
-      label: 'gists',
+      label: "gists",
       value: public_gists,
-      color: 'yellow'
-    }
-  ]
+      color: "yellow",
+    },
+  ];
 
-  return (<section className="section">
-    <Wrapper className="section-center">
-      {items.map((item) => {
-        return <Item key = {item.id} {...item}></Item>;
-      })}
-    </Wrapper>
-  </section>
+  return (
+    <section className="section">
+      <Wrapper className="section-center">
+        {items.map((item) => {
+          return <Item key={item.id} {...item}></Item>;
+        })}
+      </Wrapper>
+    </section>
   );
 };
 
-const Item = ({ icon, label, value, color}) => {
+const Item = ({ icon, label, value, color }) => {
   return (
-    <article className="item" >
+    <article className="item">
       <span className={color}>{icon}</span>
       <h3>{value}</h3>
       <p>{label}</p>
